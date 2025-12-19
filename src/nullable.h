c83e474431773e8bb2497a1c261a8b11886cd193
Artem Kravchenko <lan143@lan143.ru>
2025-12-19 22:54:04 +0300

    Add some functions to Nullable<T>

--- src/nullable.h
+++ src/nullable.h
@@ -11,5 +11,21 @@ namespace EDUtils
 
         bool Valid() const { return _valid; }
         T Value() const { return _value; }
+
+        void setValidValue(T value)
+        {
+            _valid = true;
+            _value = value;
+        }
+
+        bool operator==(Nullable& other)
+        {
+            return _value == other._value && _valid == other._valid;
+        }
+
+        bool operator!=(Nullable& other)
+        {
+            return !((*this) == other);
+        }
     };
 }
